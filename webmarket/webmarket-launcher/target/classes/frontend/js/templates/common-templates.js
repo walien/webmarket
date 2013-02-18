a8ec862e2bf9c9bde4092da7eab2901bd9ea5e2d
walien <walien@guybrush>
2013-02-18 12:59:53 +0100

    Remove target folder

--- webmarket/webmarket-launcher/target/classes/frontend/js/templates/common-templates.js
+++
@@ -1,4 +0,0 @@
-var login_modal_popup_template = "<div id='modal-login' class='modal hide fade' tabindex='-1' role='dialog' aria-labelledby='modal-label' aria-hidden='true'>"
-		+ "<div class='modal-header'><h3 id='modal-label'>Authentication</h3></div>"
-		+ "<div class='modal-body'></div>"
-		+ "<div class='modal-footer'><button class='btn' data-dismiss='modal' aria-hidden='true'>Close</button><button class='btn btn-primary' onclick='do_login();'>Login</button></div></div>";
